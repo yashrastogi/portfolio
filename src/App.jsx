50aed99b218eb919beef4602c680fe38e067caab
Yash Rastogi <yashrastogi@gmail.com>
2021-09-01 19:03:39 +0530

    Format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Cover from "./sections/Lead";
-import About from "./sections/About";
-import Experience from "./sections/Experience";
-import Education from "./sections/Education";
-import Projects from "./sections/Projects";
-import Skills from "./sections/Skills";
-import Contact from "./sections/Contact";
-import jQuerySetup from "./tools/jQuerySetup";
-import React, { useEffect } from "react";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Cover from './sections/Lead';
+import About from './sections/About';
+import Experience from './sections/Experience';
+import Education from './sections/Education';
+import Projects from './sections/Projects';
+import Skills from './sections/Skills';
+import Contact from './sections/Contact';
+import jQuerySetup from './tools/jQuerySetup';
+import React, { useEffect } from 'react';
 
 function App() {
   const sections = [About, Experience, Education, Projects, Skills, Contact];
@@ -20,7 +20,7 @@ function App() {
       <Cover />
 
       {sections.map((SectionComponent) => (
-        <SectionComponent />
+        <SectionComponent key={SectionComponent.displayName} />
       ))}
 
       <Footer />
--- src/PortfolioData.js
+++ src/PortfolioData.js
@@ -1,41 +1,41 @@
 const experienceData = [
   {
-    company: "Ernst & Young",
-    date: "January 2020 – July 2020",
-    subtitle: "Summer Trainee / Risk Advisory - Cybersecurity",
+    company: 'Ernst & Young',
+    date: 'January 2020 – July 2020',
+    subtitle: 'Summer Trainee / Risk Advisory - Cybersecurity',
     description: `Handled vulnerability management lifecycle for new nodes onboarding,
     researched SIEM use cases for telecom nodes, and audited data for
-    compliance with ISO 27001 ISMS framework.`,
-  },
+    compliance with ISO 27001 ISMS framework.`
+  }
 ];
 
 const educationData = [
   {
-    name: "NIIT University",
-    date: "2016 - Present",
-    subtitle: "Bachelor of Technology in Computer Science",
-    score: "7.85 CGPA",
+    name: 'NIIT University',
+    date: '2016 - Present',
+    subtitle: 'Bachelor of Technology in Computer Science',
+    score: '7.85 CGPA'
   },
   {
-    name: "Delhi Public School Gurgaon",
-    date: "2013-2015",
-    subtitle: "Class Xth - XIIth",
-    score: "",
-  },
+    name: 'Delhi Public School Gurgaon',
+    date: '2013-2015',
+    subtitle: 'Class Xth - XIIth',
+    score: ''
+  }
 ];
 
 const skillsData = [
-  "Flutter, Dart",
-  "React, Redux",
-  "Node.js, MongoDB",
-  "Burp Suite, Acunetix",
-  "nmap, SQLi",
-  "Java - Spring MVC, Servlets, Swing, Core, Android Development",
-  "C",
-  "JavaScript, TypeScript, jQuery",
-  "Python",
-  "MATLAB",
-  "Bash Shell",
+  'Flutter, Dart',
+  'React, Redux',
+  'Node.js, MongoDB',
+  'Burp Suite, Acunetix',
+  'nmap, SQLi',
+  'Java - Spring MVC, Servlets, Swing, Core, Android Development',
+  'C',
+  'JavaScript, TypeScript, jQuery',
+  'Python',
+  'MATLAB',
+  'Bash Shell'
 ];
 
 const aboutString = `
@@ -50,53 +50,53 @@ value to the organization.
 
 const projectsData = [
   {
-    image: process.env.PUBLIC_URL + "/images/project.jpg",
-    name: "Cloudy Cloud Storage Web SPA",
+    image: process.env.PUBLIC_URL + '/images/project.jpg',
+    name: 'Cloudy Cloud Storage Web SPA',
     description: `Built a cloud storage application with MD5 hash based
     deduplication on the MongoDB - Express - ReactJS - Node.js stack`,
-    link: "https://nu-capstone-project.github.io/cloudy/#/",
-    linkType: "project",
+    link: 'https://nu-capstone-project.github.io/cloudy/#/',
+    linkType: 'project'
   },
   {
-    image: process.env.PUBLIC_URL + "/images/project.jpg",
-    name: "Android Video Encryption and Sharing App",
+    image: process.env.PUBLIC_URL + '/images/project.jpg',
+    name: 'Android Video Encryption and Sharing App',
     description: `Created a secure video sharing app in which a user can share their
     videos protected by AES encryption.`,
-    link: "https://github.com/yashrastogi/securivo",
-    linkType: "source",
+    link: 'https://github.com/yashrastogi/securivo',
+    linkType: 'source'
   },
   {
-    image: process.env.PUBLIC_URL + "/images/project.jpg",
-    name: "Restaurant Management/Food Ordering App",
+    image: process.env.PUBLIC_URL + '/images/project.jpg',
+    name: 'Restaurant Management/Food Ordering App',
     description: `Developed a food ordering app for our local campus cafe that
     allows the cafe to take orders remotely.`,
-    link: "https://github.com/yashrastogi/CampusCafe",
-    linkType: "source",
+    link: 'https://github.com/yashrastogi/CampusCafe',
+    linkType: 'source'
   },
   {
-    image: process.env.PUBLIC_URL + "/images/project.jpg",
-    name: "Chess Web App",
+    image: process.env.PUBLIC_URL + '/images/project.jpg',
+    name: 'Chess Web App',
     description: `Developed a Chess game using web technologies which allows the
     user to play the game anywhere and on any device.`,
-    link: "https://yashrastogi.github.io/chessboard",
-    linkType: "project",
+    link: 'https://yashrastogi.github.io/chessboard',
+    linkType: 'project'
   },
   {
-    image: process.env.PUBLIC_URL + "/images/project.jpg",
-    name: "DES Swing",
+    image: process.env.PUBLIC_URL + '/images/project.jpg',
+    name: 'DES Swing',
     description: `Manually implemented the DES algorithm utilising Java and Swing
     framework that enables the user to encrypt text using DES
     algorithm.`,
-    link: "https://github.com/yashrastogi/DES-Swing",
-    linkType: "source",
+    link: 'https://github.com/yashrastogi/DES-Swing',
+    linkType: 'source'
   },
   {
-    image: process.env.PUBLIC_URL + "/images/project.jpg",
-    name: "Research & Development - Improving University network performance",
+    image: process.env.PUBLIC_URL + '/images/project.jpg',
+    name: 'Research & Development - Improving University network performance',
     description: `Performed analysis on campus wireless network and improved
     performance.`,
-    linkType: "none",
-  },
+    linkType: 'none'
+  }
 ];
 
 export { aboutString, projectsData, experienceData, educationData, skillsData };
